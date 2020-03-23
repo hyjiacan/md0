3cc1b21863f8edb1c83c385eecf9e4645782aa5a
hyjiacan <hyjiacan@163.com>
2020-03-23 17:23:16 +0800

    修复 行内代码被异常解析的问题

--- src/renders/common.js
+++ src/renders/common.js
@@ -3,7 +3,16 @@ import {mergeString} from '../util'
 export default {
   render (str, option) {
     const buffer = {}
+    let i = 0
     const tpl = str
+      // inline code
+      // 代码永远优先
+      .replace(/(`)(.+?)\1/g, function (match, group1, group2) {
+        const html = mergeString('<span class="md0-code-inline">', group2.replace(/</g, '&lt;').replace(/>/g, '&gt;'), '</span>')
+        const placeholder = `@${i++}@`
+        buffer[placeholder] = option.render ? option.render('common', html, match) : html
+        return placeholder
+      })
       // image
       .replace(/!\[(.+?)\]\((.*?)\)/g, function (match, group1, group2) {
         const html = mergeString('<img src="', group2, '" alt="', group1, '" />')
@@ -32,11 +41,6 @@ export default {
       // italic
       .replace(/([_*])(.+?)\1/g, function (match, group1, group2) {
         const html = mergeString('<i>', group2, '</i>')
-        return option.render ? option.render('common', html, match) : html
-      })
-      // inline code
-      .replace(/(`)(.+?)\1/g, function (match, group1, group2) {
-        const html = mergeString('<span class="md0-code-inline">', group2.replace(/</g, '&lt;').replace(/>/g, '&gt;'), '</span>')
         return option.render ? option.render('common', html, match) : html
       })
       // Strikethrough
